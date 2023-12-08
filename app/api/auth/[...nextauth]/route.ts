c86533aee9a683612fb7ba365bd07c6d69e2751d
Aamir Nazeer <aamirnazeerbhat@gmail.com>
2023-12-08 15:56:33 +0530

    fixed authoptions imports

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,59 +1,6 @@
-import NextAuth, { NextAuthOptions } from 'next-auth';
-import CredentialsProvider from 'next-auth/providers/credentials';
-import prisma from '@/prisma/_base';
-import bcrypt from 'bcrypt';
-import { signinValidationSchema } from '@/app/helpers/validationSchema';
+import NextAuth from 'next-auth';
 
-export const authOptions: NextAuthOptions = {
-  providers: [
-    CredentialsProvider({
-      name: 'Credentials',
-      credentials: {
-        email: { label: 'Email', type: 'email', placeholder: 'test@email.com' },
-        password: { label: 'Password', type: 'password' },
-      },
-      async authorize(credentials, req) {
-        try {
-          const validation = signinValidationSchema.safeParse(credentials);
-          if (!validation.success) return null;
-          const user = await prisma.users.findFirstOrThrow({
-            where: {
-              AND: [{ email: validation.data.email }],
-            },
-          });
-          if (bcrypt.compareSync(validation.data.password, user.password)) {
-            return user;
-          }
-          return null;
-        } catch (err) {
-          return null;
-        }
-      },
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-  pages: {
-    signIn: '/signin',
-  },
-  callbacks: {
-    async jwt({ token, user, session, trigger }) {
-      if (user) {
-        token.id = user.id;
-        token.organisation = user.organisation;
-      }
-      if (trigger === 'update' && session.name) {
-        token.name = session.name;
-      }
-      return token;
-    },
-    async session({ session, token, user }) {
-      session.user.id = token.id;
-      session.user.organisation = token.organisation;
-      delete session.user?.image;
-      return session;
-    },
-  },
-};
+import { authOptions } from '@/app/utils/authOptions';
 
 const handler = NextAuth(authOptions);
 
--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
-import { authOptions } from '../auth/[...nextauth]/route';
+import { authOptions } from '@/app/utils/authOptions';
 
 export async function GET(request: NextRequest) {
   const data = await getServerSession(authOptions);
